cef5267ff5cc41c33309edde5087cf31909d295b
Anil Madhavapeddy <anil@recoil.org>
2015-12-28 22:05:32 +0000

    try yorick

---
+++ .travis.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -ex
+
+OS=~/build/$TRAVIS_REPO_SLUG
+git clone git://github.com/ocaml/ocaml-travisci-skeleton $OS/__test
+docker run -v \
+  $OS:/repo \
+  ocaml/opam-dockerfiles:${DISTRO}_ocaml-${OCAML_VERSION} \
+  ocaml /repo/_test/scripts/travis_opam.ml
