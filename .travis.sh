fe430d809c2d03d6b2c863c1976c0c2eb5e773f1
Anil Madhavapeddy <anil@recoil.org>
2015-12-28 22:34:23 +0000

    cd correctly

--- .travis.sh
+++ .travis.sh
@@ -5,4 +5,4 @@ git clone git://github.com/ocaml/ocaml-travisci-skeleton $OS/__test
 docker run -v \
   $OS:/repo \
   ocaml/opam-dockerfiles:${DISTRO}_ocaml-${OCAML_VERSION} \
-  ocaml /repo/__test/travis_opam.ml
+  cd /repo/__test && ocaml travis_opam.ml
